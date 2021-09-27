82b25de2e3cec511606dfc6932addbe4175be55e
Vital Holopov <vitalholopov@gmail.com>
2021-09-27 13:36:51 +0300

    Fix null checking

--- packages/in_app_purchase/in_app_purchase_android/lib/src/billing_client_wrappers/sku_details_wrapper.g.dart
+++ packages/in_app_purchase/in_app_purchase_android/lib/src/billing_client_wrappers/sku_details_wrapper.g.dart
@@ -11,7 +11,8 @@ SkuDetailsWrapper _$SkuDetailsWrapperFromJson(Map json) {
     description: json['description'] as String? ?? '',
     freeTrialPeriod: json['freeTrialPeriod'] as String? ?? '',
     introductoryPrice: json['introductoryPrice'] as String? ?? '',
-    introductoryPriceMicros: json['introductoryPriceAmountMicros']?.toString() '',
+    introductoryPriceMicros: json['introductoryPriceAmountMicros']
+        ?.toString() ?? '',
     introductoryPriceCycles: json['introductoryPriceCycles'] as int? ?? 0,
     introductoryPricePeriod: json['introductoryPricePeriod'] as String? ?? '',
     price: json['price'] as String? ?? '',
